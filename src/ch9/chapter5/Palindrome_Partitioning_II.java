f642e5bc4ce50e70ff6fa70bd0ed43371275e9d9
student <student@p6s-mac10.bobst.nyu.edu>
2016-08-16 23:08:07 -0400

    he

--- src/ch9/chapter5/Palindrome_Partitioning_II.java
+++ src/ch9/chapter5/Palindrome_Partitioning_II.java
@@ -71,6 +71,7 @@ class Palindrome_Partitioning_II1 {
      for (int i = 1; i <= s.length(); i++) {
          f[i] = Integer.MAX_VALUE; // or f[i] = i
          for (int j = 0; j < i; j++) {
+        	//从j到i - 1的String是否为回文字符串， 也就是第j＋1个字符到第i个字符的string
              if (isPalindrome[j][i - 1]) {
                  f[i] = Math.min(f[i], f[j] + 1);
              }
@@ -92,13 +93,18 @@ class Palindrome_Partitioning_II1 {
  *            f[2] = f["aa"] = 0
  *            f[1] = f["a"] = 0
  *            f[0] = f[""] = -1
- *            f[i] = min{f[j] + 1}
  *            
  *            ""|aa
  *            f[""] + 1 = f["aa"]
  *            f[""] = f["aa"] - 1
  *            f[""] = 0 - 1 = -1
+ *            
  * 2, f[i] 表示第i个字符，组成的前缀，最少能分割为几个回文串
+ * 		      f[3] = f["aab"] = 2
+ *            f[2] = f["aa"] = 1
+ *            f[1] = f["a"] = 1
+ *            f[0] = f[""] = 0
+ * 	
  * 		return f[n] - 1
  */
 class Palindrome_Partitioning_II2 {
@@ -112,18 +118,22 @@ class Palindrome_Partitioning_II2 {
      return true;
  }
 
+ //用一个小于n ^ 3的方法，给出一个可以搜索是否回文串的二维数组
  private boolean[][] getIsPalindrome(String s) {
      boolean[][] isPalindrome = new boolean[s.length()][s.length()];
-
+     //单个字母的情况，认为是回文串
      for (int i = 0; i < s.length(); i++) {
          isPalindrome[i][i] = true;
      }
+     //两个字母的情况，如果两个字母相等，则认为是回文串
      for (int i = 0; i < s.length() - 1; i++) {
          isPalindrome[i][i + 1] = (s.charAt(i) == s.charAt(i + 1));
      }
 
+     //大区间依赖于小区间，所以小区间要先做
      for (int length = 2; length < s.length(); length++) {
          for (int start = 0; start + length < s.length(); start++) {
+        	 //中间的string是回文串，而两端的两个字母相同，则外侧String是回文串
              isPalindrome[start][start + length]
                  = isPalindrome[start + 1][start + length - 1] 
                 		 && s.charAt(start) == s.charAt(start + length);
@@ -161,6 +171,7 @@ class Palindrome_Partitioning_II2 {
 }
 
 //9Ch summer 2016
+//大区间依赖于小区间
 class Solution{
 	public int minCut(String s){
 		//
@@ -171,6 +182,7 @@ class Solution{
 		}
 	
 		//f[i][j] <- f[i + 1][j - 1];
+		//delta = j - i
 		//(0, n - 1)
 		for(int delta = 1; delta < n; delta++){
 			for(int i = 0; i < n; i++){
