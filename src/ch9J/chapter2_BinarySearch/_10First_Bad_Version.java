c41214d73de820534a425983773b2c5c86bd135a
student <student@p6s-mac06.bobst.nyu.edu>
2016-08-18 21:24:18 -0400

    hehe

--- src/ch9J/chapter2_BinarySearch/_10First_Bad_Version.java
+++ src/ch9J/chapter2_BinarySearch/_10First_Bad_Version.java
@@ -1,12 +1,17 @@
 package ch9J.chapter2_BinarySearch;
 /*
- * The code base version is an integer start from 1 to n. One day, someone committed a bad version in the code case, so it caused this version and the following versions are all failed in the unit tests. Find the first bad version.
+ * The code base version is an integer start from 1 to n. 
+ * One day, someone committed a bad version in the code case, 
+ * so it caused this version and the following versions are all failed in the unit tests. 
+ * Find the first bad version.
 
-You can call isBadVersion to help you determine which version is the first bad one. The details interface can be found in the code's annotation part.
+You can call isBadVersion to help you determine which version is the first bad one. 
+The details interface can be found in the code's annotation part.
 
  Notice
 
-Please read the annotation in code area to get the correct way to call isBadVersion in different language. For example, Java is SVNRepo.isBadVersion(v)
+Please read the annotation in code area to get the correct way to call isBadVersion in 
+different language. For example, Java is SVNRepo.isBadVersion(v)
 
 Have you met this question in a real interview? Yes
 Example
