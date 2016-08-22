f2afa3f3706227d200d055882ad0a7a3d0d92392
student <student@p6s-mac09.bobst.nyu.edu>
2016-08-22 14:25:52 -0400

    hj

--- src/ch9J/chapter4_DP1/_4Climbing_Stairs.java
+++ src/ch9J/chapter4_DP1/_4Climbing_Stairs.java
@@ -22,6 +22,20 @@ public class _4Climbing_Stairs {
         }
         return now;
     }
+    
+    //be careful with the edge value 
+    public int climbStairs_AC(int n) {
+        if (n <= 1){
+            return 1;
+        }
+        int[] f = new int[n + 1];
+        f[0] = 1;
+        f[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        return f[n];
+    }
 }
 
 /*
