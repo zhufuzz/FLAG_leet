71af5faa2ecb508aa11b008ae07c29e4b422fa41
student <student@p6s-mac06.bobst.nyu.edu>
2016-09-10 23:57:53 -0400

    jj

--- src/com/myjava/string/_07MyStringIndexOf.java
+++ src/com/myjava/string/_07MyStringIndexOf.java
@@ -2,21 +2,21 @@ package com.myjava.string;
  
 public class _07MyStringIndexOf {
  
-    public static void main(String[] a){
+public static void main(String[] a){
+ 
+    String str = "Use this string for testing this";
+    System.out.println("Basic indexOf() example");
+    System.out.println("Char 's' at first occurance: "+str.indexOf('s'));
+    System.out.println("String \"this\" at first occurance: "+str.indexOf("this"));
+    /**
+     * Returns the first occurance from specified start index
+     */
+    System.out.println("First occurance of char 's' from 4th index onwards : "
+                        +str.indexOf('s',4));
+    System.out.println("First occurance of String \"this\" from 6th index onwards: "
+                        +str.indexOf("this",6));
      
-        String str = "Use this string for testing this";
-        System.out.println("Basic indexOf() example");
-        System.out.println("Char 's' at first occurance: "+str.indexOf('s'));
-        System.out.println("String \"this\" at first occurance: "+str.indexOf("this"));
-        /**
-         * Returns the first occurance from specified start index
-         */
-        System.out.println("First occurance of char 's' from 4th index onwards : "
-                            +str.indexOf('s',4));
-        System.out.println("First occurance of String \"this\" from 6th index onwards: "
-                            +str.indexOf("this",6));
-         
-    }
+}
 }
 /*How to get index of a character or string from another String in java?
 
