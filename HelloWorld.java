2eec8430e5ac446d92cfdaf08a0d2353d7f82dbf
tianhui <zhufuzz@hotmail.com>
2017-01-18 15:53:50 -0500

    dftg

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Student on 12/27/16.
- */
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.print("hello world");
-    }
-
-}
--- HelloWorld2.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Student on 12/27/16.
- */
-public class HelloWorld2 {
-    public static void main(String[] args) {
-        System.out.print("hello world");
-    }
-
-}
