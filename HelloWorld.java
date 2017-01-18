259f1f0fe1bae9c828595ed424cab24a6934e7e9
tianhui <zhufuzz@hotmail.com>
2017-01-18 15:34:58 -0500

    sdfa

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,3 @@
-
 /**
  * Created by Student on 12/27/16.
  */
--- HelloWorld2.java
+++ HelloWorld2.java
@@ -1,8 +1,7 @@
-
 /**
  * Created by Student on 12/27/16.
  */
-public class HelloWorld {
+public class HelloWorld2 {
     public static void main(String[] args) {
         System.out.print("hello world");
     }
